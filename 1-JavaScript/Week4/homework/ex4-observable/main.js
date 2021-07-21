1956e102e1916459a595c7052b12ef15a2c16cdb
HackYourFuture <hackyourfuture@hackyouturesair.home>
2021-07-21 10:36:13 +0200

    Resolved and exercise 4 week4

--- 1-JavaScript/Week4/homework/ex4-observable/main.js
+++ 1-JavaScript/Week4/homework/ex4-observable/main.js
@@ -4,9 +4,15 @@ const createObservable = require('./ex4-observable');
 function consoleUpperCase(message) {
   console.log(message.toUpperCase());
 }
+function listener1(message) {
+  console.log(message.toUpperCase());
+}
 
 // Another candidate subscriber function
 function consoleLowerCase(message) {
+  console.log(message.toLowerCase());
+}
+function listener2(message) {
   console.log(message.toLowerCase());
 }
 
@@ -17,9 +23,12 @@ const observable = createObservable();
 observable.subscribe(console.log);
 observable.subscribe(consoleUpperCase);
 observable.subscribe(consoleLowerCase);
+observable.subscribe(listener1);
+observable.subscribe(listener2);
 
 // Send a message to all current subscribers
 observable.notify("Let's see what happens here!");
+observable.notify('Hi!');
 // Let's see what happens here! (console.log subscriber)
 // LET'S SEE WHAT HAPPENS HERE! (consoleUpperCase subscriber)
 // let's see what happens here! (consoleLowerCase subscriber)
