f122771c8ff7ebb77b47173de1c747ec25f395b8
Alejandro Urroz <alejandrourroz86@gmail.com>
2021-08-30 23:37:14 +0200

    Resolved ex4-pokerDiceAll

--- 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
+++ 3-UsingAPIs/Week1/homework/ex4-pokerDiceAll.js
@@ -24,9 +24,12 @@ exercise file.
 const rollDice = require('../../helpers/pokerDiceRoller');
 
 function rollTheDices() {
-  // TODO Refactor this function
   const dices = [1, 2, 3, 4, 5];
-  return rollDice(1);
+  const rollAllDices = [];
+  dices.forEach((e) => {
+    rollAllDices.push(rollDice(e));
+  });
+  return Promise.all(rollAllDices);
 }
 
 rollTheDices()
